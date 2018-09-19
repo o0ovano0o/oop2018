2acfedeabacd4fe57a8d5971b63625f5e3e4fc51
Nguyễn Thị Vân Anh <van005618@gmail.com>
2018-09-19 21:57:45 +0700

    blo blo blo

--- src/week1/Student.java
+++ src/week1/Student.java
@@ -2,9 +2,9 @@ package week1;
 
 public class Student {
 
-    // TODO: khai báo các thuộc tính cho Student
+
     private String name,id,group,email;
-    // TODO: khai báo các phương thức getter, setter cho Student
+
     public void setName(String name){this.name=name;}
     public String getName(){return name;}
     public void setId(String id){this.id=id;}
@@ -13,37 +13,23 @@ public class Student {
     public String getGroup(){return group;}
     public void setEmail(String email){this.email=email;}
     public String getEmail(){return email;}
-    /**
-     * Constructor 1
-     */
+
     public Student() {
-        // TODO:
+
         name="Student";
         id="000";
         group="INT22041";
         email="uet@vnu.edu.vn";
     }
 
-    /**
-     * Constructor 2
-     * @param n
-     * @param sid
-     * @param em
-     */
     public Student(String n, String sid, String em) {
-        // TODO:
         name=n;
         id=sid;
         email=em;
         group="INT22041";
     }
 
-    /**
-     * Constructor 3
-     * @param s
-     */
     public Student(Student s) {
-        // TODO:
         this.name=s.name;
         this.id=s.id;
         this.group=s.group;
@@ -51,8 +37,8 @@ public class Student {
     }
 
     public void getInfo() {
-        // TODO:
-        System.out.println("Name: "+this.name);
+        // em đánh dấu * cho dễ nhìn ạ
+        System.out.println("* "+"Name: "+this.name);
         System.out.println("ID: "+this.id);
         System.out.println("Group: "+this.group);
         System.out.println("Email: "+this.email);
--- src/week1/StudentManagement.java
+++ src/week1/StudentManagement.java
@@ -1,109 +1,87 @@
 package week1;
 
-import java.util.HashMap;
-import java.util.Set;
+
+import java.util.HashSet;
+
 
 public class StudentManagement {
 
-    // TODO: khai báo thuộc tính students là array chứa các đối tượng thuộc lớp Student (max. 100)
-    int max = 100;
-    private Student[] students = new Student[max];
+
+    int n = 0;
+    private Student[] students = new Student[100];
 
     public static boolean sameGroup(Student s1, Student s2) {
-        // TODO:
         return s1.getGroup().equals(s2.getGroup());
-           
+
     }
 
     void studentsByGroup() {
-        // TODO:
-        // Dùng HashMap và Set....
-//        HashMap<String, Student> map = new HashMap<String, Student>();
-//        for (int i = 0; i < max; i++) {
-//            map.put(students[i].getGroup(), students[i]);
-//        }
-//        Set<String> groups = map.keySet();
-//        for (String group : groups) {
-//            for (int i = 0; i < max; i++) {
-//                if (group.equals(students[i].getGroup()))
-//                    students[i].getInfo();
-//            }
-//        }
-        String[] groups=new String[max];
-        groups[0]=students[0].getGroup();
-        boolean test =true;
-        int z=1;
-        for(int i=1;i<max;i++){
-            test=true;
-            for(int j=i-1;j>=0;j--){
-                if(sameGroup(students[i],students[j]))
-                {  test=false;
-                   break;}
-            }
-            if(test){
-                groups[z++]= students[i].getGroup();
-            }
+        HashSet<String> hs = new HashSet<String>();
+        for (int i = 0; i < n; i++) {
+            hs.add(students[i].getGroup());
         }
-        for(int j=0;j<z;j++){
-            for(int i=0;i<max;i++){
-                if(groups[j].equals(students[i].getGroup()))
+        Object[] groups = hs.toArray();
+        for (Object group : groups) {
+            for (int i = 0; i < n; i++) {
+                if (group.equals(students[i].getGroup()))
                     students[i].getInfo();
             }
         }
-
     }
+        void removeStudent(String id){
 
-    void removeStudent(String id) {
-        // TODO:
-        for (int i = 0; i < max; i++) {
-            if (students[i].getId().equals(id)) {
-                for (int j = i; j < max - 1; j++) {
-                    students[j].setName(students[j + 1].getName());
-                    students[j].setId(students[j + 1].getId());
-                    students[j].setGroup(students[j + 1].getGroup());
-                    students[j].setEmail(students[j + 1].getEmail());
+            for (int i = 0; i < n; i++) {
+                if (students[i].getId().equals(id)) {
+                    for (int j = i; j < n - 1; j++) {
+                        students[j].setName(students[j + 1].getName());
+                        students[j].setId(students[j + 1].getId());
+                        students[j].setGroup(students[j + 1].getGroup());
+                        students[j].setEmail(students[j + 1].getEmail());
+                    }
+                    n -= 1;
+                    System.out.println("***Deleted***");
+                    return;
                 }
-                max -= 1;
-                System.out.println("Deleted");
-                return;
             }
+            System.out.println("***Not found***");
         }
-        System.out.println("Not found");
+
+        public static void main (String[] args ){
+            Student me1 = new Student();
+            me1.setGroup("INT22042");
+            Student me2 = new Student("Opppaaaa", "17020555", "iloveyou@gmail.com");
+            Student me3 = new Student(me2);
+            me1.getInfo();
+            me2.getInfo();
+            me3.getInfo();
+            System.out.println(sameGroup(me1, me2));
+            System.out.println(sameGroup(me2, me3));
+            //test studentsByGroup
+            StudentManagement sv = new StudentManagement();
+            sv.n=10;
+            for (int i = 0; i < sv.n; i++) {
+                sv.students[i] = new Student();
+                sv.students[i].setId(i + "abc");
+            }
+            for (int i = 1; i < sv.n; i += 2) {
+                sv.students[i].setGroup("INT22042");
+            }
+            System.out.println("*****original data*****");
+            for (int i = 0; i < sv.n; i++) {
+                sv.students[i].getInfo();
+            }
+            System.out.println("*****Sort by group*****");
+            sv.studentsByGroup();
+           // remove ID
+            String idrm="5abc";
+            System.out.println("***Remove "+idrm+"***");
+            sv.removeStudent(idrm);
+            for (int i = 0; i < sv.n; i++) {
+                sv.students[i].getInfo();
+            }
+
+
+        }
     }
 
-    public static void main(String[] args) {
-        // TODO:
-        Student me1 = new Student();
-        me1.setGroup("INT22042");
-        Student me2 = new Student("Opppaaaa","17020555","iloveyou@gmail.com");
-        Student me3 = new Student(me2);
-        me1.getInfo();
-        me2.getInfo();
-        me3.getInfo();
-        System.out.println(sameGroup(me1,me2));
-        System.out.println(sameGroup(me2,me3));
-//        // test studentsByGroup && removeStudent
-//        StudentManagement sv = new StudentManagement();
-//        for (int i = 0; i < sv.max; i++) {
-//            sv.students[i] = new Student();
-//            sv.students[i].setId(i + "abc");
-//        }
-//        for (int i = 1; i < sv.max; i += 2) {
-//            sv.students[i].setGroup("INT22042");
-//        }
-//
-//        for (int i = 0; i < sv.max; i++) {
-//            sv.students[i].getInfo();
-//        }
-//        System.out.println("Sort by group");
-//        sv.studentsByGroup();
-//
-//        sv.removeStudent("5abc");
-//        for (int i = 0; i < sv.max; i++) {
-//            sv.students[i].getInfo();
-//            }
-
-
-        }
-    }
 
