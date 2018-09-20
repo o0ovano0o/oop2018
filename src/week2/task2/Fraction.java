2ecbdf586b1963ad993483fa9a73ad6145fea396
Nguyễn Thị Vân Anh <van005618@gmail.com>
2018-09-20 23:16:21 +0700

    tối giản phân số

--- src/week2/task2/Fraction.java
+++ src/week2/task2/Fraction.java
@@ -1,5 +1,7 @@
 package week2.task2;
 
+import  week2.task1.Task1;
+
 public class Fraction {
 
     // TODO: khai báo các thuộc tính
@@ -25,36 +27,40 @@ public class Fraction {
     public Fraction add(Fraction other) {
         // TODO: Phương thức cộng hai phân số (this và other), trả về đối tượng Fraction mới
         if(this.denominator==other.denominator)
-            return new Fraction(this.numerator+other.numerator,other.denominator);
+            return new Fraction(this.numerator+other.numerator,other.denominator).minimals();
         else{
-            return new Fraction(this.numerator*other.denominator+this.denominator*other.numerator,this.denominator*other.denominator);
+            return new Fraction(this.numerator*other.denominator+this.denominator*other.numerator,this.denominator*other.denominator).minimals();
         }
     }
 
     public Fraction subtract(Fraction other) {
         // TODO: Phương thức trừ hai phân số (this và other), trả về đối tượng Fraction mới
         if(this.denominator==other.denominator)
-            return new Fraction(this.numerator-other.numerator,other.denominator);
+            return new Fraction(this.numerator-other.numerator,other.denominator).minimals();
         else{
-            return new Fraction(this.numerator*other.denominator-this.denominator*other.numerator,this.denominator*other.denominator);
+            return new Fraction(this.numerator*other.denominator-this.denominator*other.numerator,this.denominator*other.denominator).minimals();
         }
     }
 
     public Fraction multiply(Fraction other) {
         // TODO: Phương thức nhân hai phân số (this và other), trả về đối tượng Fraction mới
-        return new Fraction(this.numerator*other.numerator,this.denominator*other.denominator);
+        return new Fraction(this.numerator*other.numerator,this.denominator*other.denominator).minimals();
     }
 
     public Fraction divide(Fraction other) {
         // TODO: Phương thức chia hai phân số (this và other), trả về đối tượng Fraction mới
-        return new Fraction(this.numerator*other.denominator,this.denominator*other.numerator);
+        return new Fraction(this.numerator*other.denominator,this.denominator*other.numerator).minimals();
     }
     public String format(){
         return numerator+"/"+denominator;
     }
+    public Fraction minimals(){
+         int ucln =Task1.gcd(this.numerator,this.denominator);
+        return new Fraction(this.numerator/ucln,this.denominator/ucln);
+    }
     public  static  void main(String[] args){
-        Fraction a= new Fraction(4,5);
-        Fraction b = new Fraction(3,2);
+        Fraction a= new Fraction(9,5);
+        Fraction b = new Fraction(6,5);
         System.out.println(a.format()+" + "+b.format()+" = "+a.add(b).format());
         System.out.println(a.format()+" + "+b.format()+" = "+a.subtract(b).format());
         System.out.println(a.format()+" + "+b.format()+" = "+a.multiply(b).format());
