cac3bd8db3031488b94c9b248c5c1c07253b1f4c
Nguyễn Thị Vân Anh <van005618@gmail.com>
2018-11-10 09:50:59 +0700

    done task 2

--- src/week10/Task1.java
+++ src/week10/Task1.java
@@ -1,4 +1,41 @@
 package week10;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Task1 {
+    public List<String> getAllFunction(String path){
+        File file = new File(path);
+        FileReader fr = null;
+        List<String> a = new ArrayList<String>();
+        String line="";
+        String b="";
+        try {
+            fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+            while((line=br.readLine())!=null) {
+                if(line.indexOf("static")>0) {
+                    a.add(line);
+                }
+            }
+            fr.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return a;
+    }
+    public String getFinctionbyname(String name){
+        return null;
+    }
+    public static void main(String[] args) {
+        Task1 task1 = new Task1();
+        ArrayList<String> method1= (ArrayList<String>) task1.getAllFunction("C:\\Users\\Administrator\\IdeaProjects\\oop2018\\src\\week9\\Utils.java");
+        for(int i=0;i<method1.size();i++){
+            System.out.println(method1.get(i));
+        }
+    }
 }
--- src/week10/Task2.java
+++ src/week10/Task2.java
@@ -1,4 +1,41 @@
 package week10;
 
+import java.util.Random;
+
 public class Task2 {
+    double[] a = new double[1000];
+    public void initdata(){
+        Random ra = new Random();
+        double start = 1;
+        double end = 10000;
+        for(int i=0;i<a.length;i++){
+            a[i]=start + (ra.nextDouble()* (end - start));
+        }
+    }
+    public void bubbleSort() {
+        double temp;
+        for(int i=0; i<a.length-1; i++) {
+            for(int j=0; j<a.length-1-i; j++) {
+                if(a[j] > a[j+1]) {
+                    temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
+                }
+            }
+        }
+    }
+    public void printAll(){
+        for(int i=0;i<a.length;i++){
+            System.out.println(a[i]);
+        }
+    }
+    public static void main(String[] args) {
+        Task2 task2 = new Task2();
+        task2.initdata();
+        System.out.println("before");
+        task2.printAll();
+        task2.bubbleSort();
+        System.out.println("after");
+        task2.printAll();
+    }
 }
