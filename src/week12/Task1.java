3ca2e70da557249237517f05c6bdd46b05e338a2
Nguyễn Thị Vân Anh <van005618@gmail.com>
2018-11-28 22:53:08 +0700

    done

--- src/week12/Task1.java
+++ src/week12/Task1.java
@@ -1,4 +1,116 @@
 package week12;
 
+import java.util.ArrayList;
+
+interface Family {
+    public String showinfo();
+    public ArrayList<Family> getChildren();
+    public boolean add(Family c);
+}
+
+class Couple implements Family {
+    private String wife,husband;
+    private ArrayList<Family> families = new ArrayList<Family>();
+
+    public Couple(String name1, String name2){
+        this.wife = name1;
+        this.husband=name2;
+    }
+    public String getWife() {
+        return wife;
+    }
+
+    public void setWife(String wife) {
+        this.wife = wife;
+    }
+
+    public String getHusband() {
+        return husband;
+    }
+
+    public void setHusband(String husband) {
+        this.husband = husband;
+    }
+    public String showinfo() {
+        String s ="("+ wife + " and " + husband+":" ;
+        for (Family child: getChildren())
+            s += " " + child.showinfo()+",";
+        return s+")" ;
+    }
+
+    public ArrayList<Family> getChildren()       { return families; }
+    public boolean add(Family c)        { return families.add(c); }
+
+}
+
+
+class Single implements Family {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Single(String name)              { this.name = name; }
+
+    public String showinfo()                   { return name; }
+    public ArrayList<Family> getChildren()       { return null; }
+    public boolean add(Family c)        { return false; }
+
+}
+
 public class Task1 {
+
+    public static String showSingle(Family i) {
+        String s ="" ;
+        for (Family child: i.getChildren()){
+            if(child instanceof Single)
+                s += ((Single) child).getName()+" ";
+            else
+                s+=showSingle(child)+" ";
+        }
+        return s;
+    }
+
+
+    public static void showCouple(Family i) {
+        String s = ((Couple) i).getHusband()+" and "+((Couple) i).getWife()+" ;";
+        for (Family child: i.getChildren()){
+            if(child instanceof Couple)
+                s += ((Couple) child).getWife()+" and " +((Couple) child).getHusband()+" ;";
+        }
+        System.out.println("Couple: "+s);
+    }
+
+
+
+    public static String showNew(Family m) {
+        String s ="" ;
+        for (Family child: m.getChildren()){
+            if(child instanceof Single)
+                s += ((Single) child).getName()+" ";
+            else
+                s=showSingle(child);
+        }
+        return s;
+    }
+    public static void main(String[] args) {
+        Couple jamehana = new Couple("Jame","Hana");
+        Single ryan = new Single("Ryan");
+        Couple kaijen = new Couple("Kai","Jen");
+        jamehana.add(ryan);
+        jamehana.add(kaijen);
+        Single mice = new Single("Mice");
+        Single kitty = new Single("Kitty");
+        kaijen.add(mice);
+        kaijen.add(kitty);
+        System.out.println(jamehana.showinfo());
+        System.out.println("Single: "+showSingle(jamehana));
+        showCouple(jamehana);
+        System.out.println("New:"+showNew(jamehana));
+    }
 }
--- src/week12/Task2.java
+++ src/week12/Task2.java
@@ -1,4 +1,53 @@
 package week12;
 
+interface Sort
+{
+    public void sort(int arr[], int n);
+}
+
+class BubbleSort implements Sort {
+
+    @Override
+    public void sort(int arr[], int n) {
+       for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
+
+class SelectionSort implements Sort{
+
+    @Override
+    public void sort(int[] arr, int n) {
+            int i,imin,j,temp;
+            for (i=0; i<=n-2; i++)
+            {
+                imin = arr[i];
+                for (j=i+1; j<=n-1; j++)
+                    if (arr[j] < imin)
+                    {
+                        imin = arr[j];
+                        temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+            }
+        }
+    }
+
 public class Task2 {
+    public static void main(String[] args) {
+        int[] arr={3,2,5,7,9,5,3,6,8,9,4,1,0,7};
+        BubbleSort bs= new BubbleSort();
+        bs.sort(arr,arr.length);
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
 }
